7832bfef033f93b892f923ad5632923fe3010af2
UlfetZakirli <ulfetzakirli@gmail.com>
2022-09-25 18:08:38 +0400

    react-presentation: React.memo vs useCallback

---
+++ presentation1/src/data.js
@@ -0,0 +1,8 @@
+
+const phones = [
+    {id:1, name: "iPhone 12", price: 14000 },
+    {id:2, name: "Galaxy S20", price: 6500 },
+    {id: 3, name: "Huawei P40", price: 10000 },
+  ];
+  
+export default phones
