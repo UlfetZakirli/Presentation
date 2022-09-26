51b2666d70813ac869497022e09da309632c5d78
UlfetZakirli <ulfetzakirli@gmail.com>
2022-09-26 15:00:35 +0400

    react task: useMemo

---
+++ presentation2/src/App.js
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react';
+
+const users = [
+  { id: 'a', name: 'Robin' },
+  { id: 'b', name: 'Dennis' },
+  { id: 'c', name: 'Jim' },
+  { id: 'd', name: 'Adam' },
+  { id: 'e', name: 'Bob' },
+  { id: 'f', name: 'Jhon' },
+  { id: 'j', name: 'Marta' },
+];
+
+const App = () => {
+  const [text, setText] = React.useState('');
+  const [search, setSearch] = React.useState('');
+
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearch(text);
+  };
+
+  const filteredUsers = useMemo(()=>users.filter((user) => {
+    console.log('filter function is running...');
+    return user.name.toLowerCase().includes(search.toLowerCase());
+  }),[search])
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={handleText} />
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
+
+      <List list={filteredUsers} />
+    </div>
+  );
+};
+
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <ListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const ListItem = ({ item }) => {
+  return <li>{item.name}</li>;
+};
+
+export default App;
